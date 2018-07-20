a2619b73a0ab1b289a1ef1d66604e8f229a7cdfd
Sergey Cheparev <sergey@cheparev.com>
2018-07-20 16:29:14 +0200

    Initial wagtail structure

---
+++ portfolio/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'nhph9jxo%9c3npo7y9oli)=v1r6#fdf!*1*#kbe1uq@09%y&dc'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
