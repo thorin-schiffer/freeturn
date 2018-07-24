8f7c262f0cea20bccabf4ae6a1ec80e71c243836
Sergey Cheparev <sergey@cheparev.com>
2018-07-24 16:48:06 +0200

    Fixes for home page

--- context_processors.py
+++ context_processors.py
@@ -4,7 +4,6 @@ from home.models import HomePage, PortfolioPage
 def menu_items(request):
     return {
         'menu_items': [
-            HomePage.objects.last(),
             PortfolioPage.objects.last()
         ]
     }
