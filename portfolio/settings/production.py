41fb43e5e46d4f549d55f2b0cde6c48ac3d79e52
Sergey Cheparev <sergey@cheparev.com>
2018-08-01 15:15:34 +0200

    Fixing allowed hosts on production config

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -9,7 +9,7 @@ DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
+ALLOWED_HOSTS = ['*']
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 try:
