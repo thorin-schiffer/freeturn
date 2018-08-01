44ffc0bd1d6869eb4379c711c0c82c45b48d785c
Sergey Cheparev <sergey@cheparev.com>
2018-08-01 15:05:39 +0200

    Default config on postgres for production

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -2,6 +2,13 @@ from .base import *
 
 DEBUG = False
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'portfolio_db',
+    }
+}
+
 try:
     from .local import *
 except ImportError:
