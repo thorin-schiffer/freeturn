1f342c534eb04e6864b61308bb26e1ff57e5a724
Sergey Cheparev <sergey@cheparev.com>
2018-08-01 15:05:49 +0200

    Installed django toolbelt

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -2,12 +2,13 @@ from .base import *
 
 DEBUG = False
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'portfolio_db',
-    }
-}
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 try:
     from .local import *
