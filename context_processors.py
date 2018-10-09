38ab5a9436f4177226c27db8fe9eab85943a4eb6
Sergey Cheparev <sergey.cheparev.external@zlabels.de>
2018-10-09 16:34:54 +0200

    Made menu identifyable by dict keys

--- context_processors.py
+++ context_processors.py
@@ -1,10 +1,10 @@
-from home.models import HomePage, PortfolioPage, TechnologiesPage
+from home.models import PortfolioPage, TechnologiesPage
 
 
 def menu_items(request):
     return {
-        'menu_items': [
-            PortfolioPage.objects.last(),
-            TechnologiesPage.objects.last(),
-        ]
+        'menu_items': {
+            "portfolio": PortfolioPage.objects.last(),
+            "technology": TechnologiesPage.objects.last(),
+        }
     }
