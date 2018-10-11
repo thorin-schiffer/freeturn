b302f1a14d81ee594d9dacb1624b0df2579cbd02
Sergey Cheparev <sergey.cheparev.external@zlabels.de>
2018-10-11 12:44:10 +0200

    Depoly and push task

--- tasks.py
+++ tasks.py
@@ -2,5 +2,8 @@ import invoke
 
 
 @invoke.task
-def deploy(context):
-    context.run("git push heroku develop:master")
+def deploy(context, repo=True):
+    if repo:
+        context.run("git push origin && git push heroku develop:master")
+    else:
+        context.run("git push heroku develop:master")
