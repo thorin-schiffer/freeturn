106c9cde9ab4bdfad14d4f4e8e1bbecf357cef7c
Sergey Cheparev <sergey@cheparev.com>
2018-10-18 17:28:38 +0200

    Added contact page

--- home/models.py
+++ home/models.py
@@ -4,7 +4,9 @@ from django.db.models import Count
 from modelcluster.contrib.taggit import ClusterTaggableManager
 from modelcluster.fields import ParentalKey
 from taggit.models import TaggedItemBase, Tag
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel, FieldRowPanel
+from wagtail.contrib.forms.edit_handlers import FormSubmissionsPanel
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
 from wagtail.core.fields import RichTextField
 from wagtail.core.models import Page
 from wagtail.images.edit_handlers import ImageChooserPanel
@@ -186,7 +188,11 @@ class TechnologyInfo(models.Model):
         verbose_name_plural = 'technologies'
 
 
-class ContactPage(Page):
+class FormField(AbstractFormField):
+    page = ParentalKey('ContactPage', on_delete=models.CASCADE, related_name='form_fields')
+
+
+class ContactPage(AbstractEmailForm):
     background = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
@@ -194,3 +200,19 @@ class ContactPage(Page):
         on_delete=models.SET_NULL,
         related_name='+'
     )
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+
+    content_panels = AbstractEmailForm.content_panels + [
+        FormSubmissionsPanel(),
+        FieldPanel('intro', classname="full"),
+        InlinePanel('form_fields', label="Form fields"),
+        FieldPanel('thank_you_text', classname="full"),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address', classname="col6"),
+                FieldPanel('to_address', classname="col6"),
+            ]),
+            FieldPanel('subject'),
+        ], "Email"),
+    ]
