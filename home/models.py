b8f7923eebdb85f30fae6970b27628c90c95094c
Sergey Cheparev <sergey@cheparev.com>
2018-10-18 13:28:33 +0200

    Added picture

--- home/models.py
+++ home/models.py
@@ -24,6 +24,16 @@ class HomePage(Page):
                              help_text="Claim text placed under the name",
                              default="Freelance python developer")
     title_color = RGBColorField(default="#FFFFFF")
+
+    picture = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text="My picture"
+    )
+
     subpage_types = [
         'home.PortfolioPage',
         'home.TechnologiesPage'
@@ -31,6 +41,7 @@ class HomePage(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel('title_color'),
+        ImageChooserPanel('picture'),
         ImageChooserPanel('background'),
     ]
 
