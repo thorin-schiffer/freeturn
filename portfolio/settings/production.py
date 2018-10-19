cbbf48c95d233da359121f63daf288128a365467
Sergey Cheparev <sergey@cheparev.com>
2018-10-19 14:59:35 +0200

    Added sentry init in prod

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -20,6 +20,13 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn=os.getenv("SENTRY_DSN"),
+    integrations=[DjangoIntegration()]
+)
 
 try:
     from .local import *
