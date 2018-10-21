50dd8b47a29f52b09446de9e92e768517201980d
Sergey Cheparev <sergey@cheparev.com>
2018-10-21 16:04:16 +0200

    Revert "Removed turbolinks"
    This reverts commit 58e55d8

--- portfolio/settings/base.py
+++ portfolio/settings/base.py
@@ -24,6 +24,7 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 
 INSTALLED_APPS = [
     'home',
+    'turbolinks',
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
     'wagtail.embeds',
@@ -55,6 +56,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'turbolinks.middleware.TurbolinksMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
