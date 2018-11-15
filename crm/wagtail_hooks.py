92ba3e039aead4147f92a8e3266f069ee40eba5a
Sergey Cheparev <sergey@cheparev.com>
2018-11-15 19:34:33 +0100

    Added created timestamp to messages list

--- crm/wagtail_hooks.py
+++ crm/wagtail_hooks.py
@@ -191,8 +191,9 @@ class MessageAdmin(ModelAdmin):
     model = ProjectMessage
     menu_icon = 'fa-envelope-open'
     menu_label = 'Messages'
-    list_display = ['subject', 'author', 'project']
+    list_display = ['subject', 'author', 'project', 'created']
     list_filter = ['project', 'author']
+    ordering = ['-created']
     inspect_view_enabled = True
     inspect_view_fields = ['project', 'subject', 'from_address', 'html']
 
