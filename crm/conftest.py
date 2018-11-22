e7234dfa5cd021163efdc97bacd1db51706c84c1
Sergey Cheparev <sergey@cheparev.com>
2018-11-22 22:09:49 +0100

    Employee factory + dependent factories

--- crm/conftest.py
+++ crm/conftest.py
@@ -2,3 +2,6 @@ from pytest_factoryboy import register
 from crm import factories
 
 register(factories.CityFactory)
+register(factories.EmployeeFactory)
+register(factories.ChannelFactory)
+register(factories.RecruiterFactory)
--- crm/factories.py
+++ crm/factories.py
@@ -8,3 +8,37 @@ class CityFactory(factory.DjangoModelFactory):
 
     class Meta:
         model = models.City
+
+
+class ChannelFactory(factory.DjangoModelFactory):
+    name = factory.Sequence(lambda n: f"channel {n}")
+    url = factory.Faker('uri')
+
+    class Meta:
+        model = models.Channel
+
+
+class BaseCompanyFactory(factory.DjangoModelFactory):
+    name = factory.Faker('company')
+    location = factory.SubFactory(CityFactory)
+    channel = factory.SubFactory(ChannelFactory)
+    url = factory.Faker('uri')
+
+    class Meta:
+        abstract = True
+
+
+class RecruiterFactory(BaseCompanyFactory):
+    class Meta:
+        model = models.Recruiter
+
+
+class EmployeeFactory(factory.DjangoModelFactory):
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+    telephone = factory.Faker('phone_number')
+
+    company = factory.SubFactory(RecruiterFactory)
+
+    class Meta:
+        model = models.Employee
