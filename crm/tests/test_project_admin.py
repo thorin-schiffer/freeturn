c75638622616757d25b47041c4e149ef0b49b62d
Sergey Cheparev <sergey@cheparev.com>
2018-11-29 14:41:01 +0100

    Webtest assuming 200 by default

--- crm/tests/test_project_admin.py
+++ crm/tests/test_project_admin.py
@@ -13,7 +13,6 @@ def test_index_action(admin_app,
     assert len(inputs) == 1
 
     r = r.forms[1].submit().follow()
-    assert r.status_code == 200
 
     project.refresh_from_db()
     assert project.state == 'stopped'
@@ -25,7 +24,6 @@ def test_inspect(admin_app,
                  project):
     url = reverse('crm_project_modeladmin_inspect', kwargs={'instance_pk': project.pk})
     r = admin_app.get(url)
-    assert r.status_code == 200
 
 
 @pytest.mark.django_db
@@ -37,5 +35,4 @@ def test_inspect_blank(admin_app, project_factory):
         daily_rate=None
     )
     url = reverse('crm_project_modeladmin_inspect', kwargs={'instance_pk': project.pk})
-    r = admin_app.get(url)
-    assert r.status_code == 200
+    admin_app.get(url)
