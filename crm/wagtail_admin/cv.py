f5e80a5510b1660b7dd726dabdcb29e9114746d7
Sergey Cheparev <sergey@cheparev.com>
2018-12-06 21:14:56 +0100

    More automatic prefill

--- crm/wagtail_admin/cv.py
+++ crm/wagtail_admin/cv.py
@@ -2,19 +2,25 @@ from wagtail.contrib.modeladmin.options import ModelAdmin
 from wagtail.contrib.modeladmin.views import CreateView
 
 from crm.models import CV, CVGenerationSettings
+from home.models import HomePage
 
 
 class CreateCVView(CreateView):
     def get_initial(self):
         site = self.request.site
+        user = self.request.user
         settings = CVGenerationSettings.for_site(site)
+        home = HomePage.objects.live().first()
         return {
             "title": settings.default_title,
             "experience_overview": settings.default_experience_overview,
             "education_overview": settings.default_education_overview,
             "contact_details": settings.default_contact_details,
             "languages_overview": settings.default_languages_overview,
-            "rate_overview": settings.default_rate_overview
+            "rate_overview": settings.default_rate_overview,
+            "full_name": f"{user.first_name} {user.last_name}",
+            "earliest_available": home.earliest_available,
+            "picture": home.picture
         }
 
 
