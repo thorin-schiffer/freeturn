04b952cd6afe0482e6417f394453622411d64929
Sergey Cheparev <sergey@cheparev.com>
2018-12-06 19:40:23 +0100

    Default site testing fixture for crm app

--- crm/conftest.py
+++ crm/conftest.py
@@ -2,6 +2,8 @@ from pytest_factoryboy import register
 from crm import factories
 import pytest
 
+from home.factories import SiteFactory
+
 register(factories.CityFactory)
 register(factories.EmployeeFactory)
 register(factories.ChannelFactory)
@@ -13,9 +15,15 @@ register(factories.MessageFactory)
 register(factories.ProjectMessageFactory)
 register(factories.UserFactory)
 register(factories.AdminFactory, "admin_user")
+register(SiteFactory)
 
 
 @pytest.fixture
 def admin_app(django_app, admin_user):
     django_app.set_user(admin_user)
     return django_app
+
+
+@pytest.fixture
+def default_site(site_factory):
+    return site_factory.create(is_default_site=True)
