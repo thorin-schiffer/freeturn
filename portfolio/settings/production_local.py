8abc4f498b701b2309e100edf197753052e2e19f
Sergey Cheparev <sergey@cheparev.com>
2018-12-09 17:01:47 +0100

    Moved local path to wkhtmltopdf 0.12.5 to local settings overriding

--- portfolio/settings/production_local.py
+++ portfolio/settings/production_local.py
@@ -13,8 +13,6 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-WKHTMLTOPDF_CMD = '/usr/local/bin/wkhtmltopdf'
-
 try:
     from .local import *
 except ImportError:
