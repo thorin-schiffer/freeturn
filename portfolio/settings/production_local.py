fc2c50a24d60120dc695f105b5fba8308f722c97
Sergey Cheparev <sergey@cheparev.com>
2018-12-09 16:59:41 +0100

    Local production setup config

---
+++ portfolio/settings/production_local.py
@@ -0,0 +1,21 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+SECRET_KEY = os.environ.get("SECRET_KEY")
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+AWS_STORAGE_BUCKET_NAME = 'cheparev-portfolio-dev'
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+WKHTMLTOPDF_CMD = '/usr/local/bin/wkhtmltopdf'
+
+try:
+    from .local import *
+except ImportError:
+    pass
