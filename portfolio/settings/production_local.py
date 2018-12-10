68b5eb404c189f44dda118c7bb2ef6faf91eaaa7
Sergey Cheparev <sergey@cheparev.com>
2018-12-10 14:54:31 +0100

    Enabled production local env with django debug toolbar

--- portfolio/settings/production_local.py
+++ portfolio/settings/production_local.py
@@ -1,6 +1,7 @@
 from .base import *
 
 DEBUG = True
+INTERNAL_IPS = ['127.0.0.1']
 
 ALLOWED_HOSTS = ['*']
 SECRET_KEY = os.environ.get("SECRET_KEY")
@@ -12,6 +13,10 @@ AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+INSTALLED_APPS += ['debug_toolbar']
+MIDDLEWARE = MIDDLEWARE + ['debug_toolbar.middleware.DebugToolbarMiddleware']
+
 
 try:
     from .local import *
