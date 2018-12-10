a2fbb0c25e6a217091d76dd671a137cf4391a4a1
Sergey Cheparev <sergey@cheparev.com>
2018-12-10 14:55:05 +0100

    Don't process menu in admin

--- context_processors.py
+++ context_processors.py
@@ -1,6 +1,8 @@
 from home.models import PortfolioPage, TechnologiesPage, ContactPage
+from utils import disabled_in_admin
 
 
+@disabled_in_admin
 def menu_items(request):
     return {
         'menu_items': {
