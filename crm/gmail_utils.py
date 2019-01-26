24ab7a3bc2e59494d1b034661025d7ef7ce365d8
Sergey Cheparev <sergey@cheparev.com>
2019-01-26 17:21:49 +0100

    Moved actual gmail api requests to a separate function

--- crm/gmail_utils.py
+++ crm/gmail_utils.py
@@ -34,28 +34,32 @@ def parse_message(message):
     return result
 
 
+def get_raw_messages(user):
+    usa = user.social_auth.get(provider='google-oauth2')
+    service = discovery.build('gmail', 'v1', credentials=Credentials(usa))
+
+    labels = service.users().labels().list(userId='me').execute()
+    try:
+        label_id = next(
+            label_info['id'] for label_info in labels['labels'] if label_info['name'] == settings.MAILBOX_LABEL
+        )
+    except StopIteration:
+        logger.error(f"Can't find label with {settings.MAILBOX_LABEL}", code=127)
+        return []
+
+    # INBOX means a message is not archived
+    mail = service.users().messages().list(userId='me', labelIds=[label_id, "INBOX"]).execute()
+    message_ids = [message['id'] for message in mail['messages']]
+    return [
+        service.users().messages().get(userId='me',
+                                       id=message_id, format='raw').execute()
+        for message_id in message_ids
+    ]
+
+
 def sync():
     for user in get_user_model().objects.exclude(social_auth=None):
-        usa = user.social_auth.get(provider='google-oauth2')
-        service = discovery.build('gmail', 'v1', credentials=Credentials(usa))
-
-        labels = service.users().labels().list(userId='me').execute()
-        try:
-            label_id = next(
-                label_info['id'] for label_info in labels['labels'] if label_info['name'] == settings.MAILBOX_LABEL
-            )
-        except StopIteration:
-            logger.error(f"Can't find label with {settings.MAILBOX_LABEL}", code=127)
-            continue
-
-        # INBOX means a message is not archived
-        mail = service.users().messages().list(userId='me', labelIds=[label_id, "INBOX"]).execute()
-        message_ids = [message['id'] for message in mail['messages']]
-        raw_messages = [
-            service.users().messages().get(userId='me',
-                                           id=message_id, format='raw').execute()
-            for message_id in message_ids
-        ]
+        raw_messages = get_raw_messages(user)
         parsed_messages = [
             parse_message(raw_message) for raw_message in raw_messages
         ]
