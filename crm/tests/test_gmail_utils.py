273a98972f0b62c2ede562967230912fc37b9853
Sergey Cheparev <sergey@cheparev.com>
2019-05-09 16:11:04 +0200

    Gmail service fixture sketch, mocking labels

--- crm/tests/test_gmail_utils.py
+++ crm/tests/test_gmail_utils.py
@@ -1,11 +1,23 @@
-from google.oauth2.credentials import Credentials
-from googleapiclient.http import HttpMock
+import json
+import os
+
+import pytest
 
 from crm.gmail_utils import get_raw_messages
 
 
-def test_get_raw_messages():
-    creds = Credentials("xyz")
-    # https://developers.google.com/api-client-library/python/guide/mocks
-    http = HttpMock('books-discovery.json', {'status': '200'})
-    get_raw_messages(creds, http)
+@pytest.fixture
+def gmail_service(mock):
+    service = mock.Mock()
+    from django.conf import settings
+    path = os.path.join(
+        settings.BASE_DIR, "crm", "tests", "data", "gmapi_labels_response.json"
+    )
+    with open(path, "r") as f:
+        data = json.load(f)
+    mock.patch('crm.gmail_utils.get_labels', lambda s: data)
+    return service
+
+
+def test_get_raw_messages(gmail_service):
+    get_raw_messages(gmail_service)
