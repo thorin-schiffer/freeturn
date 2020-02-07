4da71b28a6dccafcdd572e88260674c88ea438a5
Sergey Cheparev <sergey@cheparev.com>
2020-02-07 14:01:06 +0100

    Made uwsgi looking for dotenv

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -10,6 +10,11 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import dotenv
+
+dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
+
+dotenv.load_dotenv(dotenv_path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings.dev")
 
