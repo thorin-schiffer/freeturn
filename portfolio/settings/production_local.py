eda293e00e88e01c266cb380e8129fc94cf9f6fb
Sergey Cheparev <sergey@cheparev.com>
2020-04-13 13:44:12 +0200

    Moved aws s3 bucket name to dotenv config

--- portfolio/settings/production_local.py
+++ portfolio/settings/production_local.py
@@ -7,7 +7,7 @@ ALLOWED_HOSTS = ['*']
 SECRET_KEY = os.environ.get("SECRET_KEY")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-AWS_STORAGE_BUCKET_NAME = 'cheparev-portfolio-dev'
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
