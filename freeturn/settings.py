f5fd0862d2982e2bd4fac24b93b14fc17582a201
Sergey Cheparev <sergey@cheparev.com>
2020-04-23 14:19:38 +0200

    Installed wagtail storages

--- freeturn/settings.py
+++ freeturn/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'taggit',
     'colorful',
     'storages',
+    'wagtail_storages',
     'ajax_select',
     'wkhtmltopdf',
     'analytical',
@@ -219,6 +220,8 @@ if AWS_STORAGE_BUCKET_NAME:
     AWS_ACCESS_KEY_ID = env.str("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = env.str("AWS_SECRET_ACCESS_KEY")
     AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+    AWS_DEFAULT_ACL = "private"
+    AWS_S3_FILE_OVERWRITE = False
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 DJANGO_REDIS_IGNORE_EXCEPTIONS = True
