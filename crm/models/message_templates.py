cf59f99f623f1f9cc7e0d55ebc515756782570e9
Thorin Schiffer <thorin@schiffer.pro>
2021-07-20 14:21:47 +0200

    Added project message template model

---
+++ crm/models/message_templates.py
@@ -0,0 +1,19 @@
+from django.db.models import CharField
+from django_extensions.db.models import TimeStampedModel
+from wagtail.core.fields import RichTextField
+
+from crm.models import Project
+
+
+def get_project_state_transitions():
+    transitions = list(Project._meta.get_field('state').get_all_transitions(Project))
+    return [(transition.name, transition.name.upper()) for transition in transitions]
+
+
+class MessageTemplate(TimeStampedModel):
+    text = RichTextField(help_text='Text of the message')
+    state_transition = CharField(max_length=50,
+                                 choices=get_project_state_transitions(),
+                                 help_text='Project state transition this message template '
+                                           'will be associated with, if any',
+                                 unique=True, null=True, blank=True, default=None)
