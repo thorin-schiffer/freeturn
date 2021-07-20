33d709b05659c0ca4f93ce68eb055ed7d72ab244
Thorin Schiffer <thorin@schiffer.pro>
2021-07-20 16:38:55 +0200

    Templates are not unique, they can repeat for different languages

--- crm/models/message_templates.py
+++ crm/models/message_templates.py
@@ -16,7 +16,7 @@ class MessageTemplate(TimeStampedModel):
                                  choices=get_project_state_transitions(),
                                  help_text='Project state transition this message template '
                                            'will be associated with, if any',
-                                 unique=True, null=True, blank=True, default=None)
+                                 null=True, blank=True, default=None)
     name = CharField(max_length=100)
 
     def __str__(self):
