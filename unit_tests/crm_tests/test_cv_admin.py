2676d615260d324d27e1fbc4c5795a62a81e31d6
Thorin Schiffer <thorin@schiffer.pro>
2021-08-04 17:30:37 +0200

    First working version for cv edit test

--- unit_tests/crm_tests/test_cv_admin.py
+++ unit_tests/crm_tests/test_cv_admin.py
@@ -1,8 +1,12 @@
+import json
+
 import pytest
 from django.urls import reverse
+from wagtail.tests.utils.form_data import rich_text
+from webtest import Field
 
 from crm.models.settings import CVGenerationSettings
-from home.factories import ProjectPageFactory, PortfolioPageFactory
+from home.factories import ProjectPageFactory, PortfolioPageFactory, TechnologyFactory
 
 
 @pytest.mark.django_db
@@ -44,4 +48,32 @@ def test_create_empty_project(admin_app, admin_user, default_site):
     url = f"{reverse('crm_cv_modeladmin_create')}"
     r = admin_app.get(url)
     form = r.forms[1]
+    assert form.submit().maybe_follow().status_code == 200
+
+
+def add_field_for_autocomplete(form, value, name):
+    field = Field(form, tag='input', value=json.dumps(value), name=name, pos=20)
+    form.fields[name] = [field]
+    form.field_order.append((name, field))
+
+
+@pytest.mark.django_db
+def test_edit(admin_app, cv_with_relevant):
+    PortfolioPageFactory()
+    url = reverse('crm_cv_modeladmin_edit', kwargs={'instance_pk': cv_with_relevant.pk})
+    r = admin_app.get(url)
+    form = r.forms[1]
+
+    form['experience_overview'] = rich_text('test')
+    form['education_overview'] = rich_text('test')
+
+    form['contact_details'] = rich_text('test')
+    form['languages_overview'] = rich_text('test')
+    form['rate_overview'] = rich_text('test')
+    form['working_permit'] = rich_text('test')
+
+    add_field_for_autocomplete(form, [{'pk': ProjectPageFactory().pk}], 'relevant_project_pages')
+    add_field_for_autocomplete(form, [{'pk': ProjectPageFactory().pk}], 'highlighted_project_pages')
+    add_field_for_autocomplete(form, [{'pk': TechnologyFactory().pk}], 'relevant_skills')
+
     assert form.submit().maybe_follow().status_code == 200
